d6073cbd409eb9af7a52552dbf5e9097927a68fd
fishca <sergrud@mail.ru>
2026-05-12 14:57:55 +0300

    Перенос в core остальных файлов

--- core/cmake/V8ReaderMetadataLegacy.cmake
+++ core/cmake/V8ReaderMetadataLegacy.cmake
@@ -33,4 +33,47 @@ set(V8READER_METADATA_LEGACY_SOURCES
 
 	# P3: metadata loading/orchestration
 	src/metadata/MetaDataManager.cpp
+
+	# Remaining metadata object implementations (BaseMetadataObject descendants)
+	src/metadata/Bots.cpp
+	src/metadata/ChartOfCharacteristicTypes.cpp
+	src/metadata/ChartOfAccounts.cpp
+	src/metadata/CommonCommands.cpp
+	src/metadata/CommonModules.cpp
+	src/metadata/CommonAttributes.cpp
+	src/metadata/CommandGroups.cpp
+	src/metadata/CommonForms.cpp
+	src/metadata/FunctionalOptions.cpp
+	src/metadata/FunctionalOptionsParameters.cpp
+	src/metadata/DefinedTypes.cpp
+	src/metadata/EventSubscriptions.cpp
+	src/metadata/CommonPictures.cpp
+	src/metadata/FilterCriteria.cpp
+	src/metadata/CommonTemplates.cpp
+	src/metadata/HTTPServices.cpp
+	src/metadata/ExternalDataSources.cpp
+	src/metadata/IntegrationServices.cpp
+	src/metadata/Interfaces.cpp
+	src/metadata/Langs.cpp
+	src/metadata/ScheduledJobs.cpp
+	src/metadata/Roles.cpp
+	src/metadata/Styles.cpp
+	src/metadata/StyleItems.cpp
+	src/metadata/SettingsStorages.cpp
+	src/metadata/SessionParameters.cpp
+	src/metadata/TConstants.cpp
+	src/metadata/Subsystem.cpp
+	src/metadata/WebSocketClients.cpp
+	src/metadata/WebServices.cpp
+	src/metadata/XDTOPackages.cpp
+	src/metadata/WSReferences.cpp
+
+	# Legacy metadata support classes
+	src/metadata/Enums.cpp
+	src/metadata/Numerators.cpp
+	src/metadata/Sequences.cpp
+	src/metadata/MetaObject.cpp
+	src/metadata/Property.cpp
+	src/metadata/CommonMetadataObject.cpp
+	src/metadata/MDObject.cpp
 )
